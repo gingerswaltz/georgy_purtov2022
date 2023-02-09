0e14576a245d423377e6af1af322dd52efad0211
gingerswaltz <goshan712@mail.ru>
2023-02-09 23:28:12 +0900

    SAOD Final version
    all ready

--- SAOD1/SAOD1.cpp
+++ SAOD1/SAOD1.cpp
@@ -3,114 +3,147 @@
 #include <exception>
 #include <string>
 #include <fstream>
+#include <sstream>
+
 using namespace std;
 
 
-class itemPricement 
+class itemPricement
 {
 private:
-	string itemName="";
-	float itemPrice=0.0;
-	int itemCount = 1;
+  string itemName = "";
+  float itemPrice = 0.0;
+  int itemCount = 1;
 
 public:
-	
-	itemPricement() 
-	{
-	}
-	void setName(string& const s1) 
-	{
-		if (s1 == "") throw exception("Name is empty");
-		this->itemName = s1;
-	}
 
-	string const getName ()
-	{
-		return this->itemName;
-	}
+    itemPricement ()
+  {
+  }
+  void setName (string & s1)
+  {
+    if (s1 == "")
+      throw exception ();
+    this->itemName = s1;
+  }
 
-	void setPrice(float value)
-	{
-		if ((value < 0) || (value == NULL))
-			throw exception("Price is invalid");
+  string const getName ()
+  {
+    return this->itemName;
+  }
 
-		itemPrice = value;
+  void setPrice (float value)
+  {
+    if ((value < 0) || (value == NULL))
+      throw exception ();
 
-	}
+    itemPrice = value;
 
-	float const getPrice()
-	{
-		return this->itemPrice;
-	}
+  }
 
-	void setCount(int value)
-	{
-		if ((value < 0) || (value == NULL))
-			throw exception("Count is invalid");
-		itemCount = value;
-	}
+  float const getPrice ()
+  {
+    return this->itemPrice;
+  }
 
-	int const getCount()
-	{
-		return this->itemCount;
-	}
+  void setCount (int value)
+  {
+    if ((value < 0) || (value == NULL))
+      throw exception ();
+    itemCount = value;
+  }
 
-	float calcChange(float value)
-	{
-		
-		if (value - itemPrice>0)
-		return value - itemPrice;
-	}
+  int const getCount ()
+  {
+    return this->itemCount;
+  }
+
+  float calcChange (float value)
+  {
+
+    if (value - itemPrice > 0)
+      return value - itemPrice;
+  }
+
+  string const ToString ()
+  {
+    ostringstream ost;
+    ost << "Name of item: " << this->itemName
+      << "\t" << "Item count: " << this->itemCount
+      << "\t" << "Price of 1 item: " << this->itemPrice
+      << "$\t" << "Final price: " << (this->getPrice()) *
+      (this->getCount()) << '\n';
+    string s = ost.str ();
+    return s;
+  }
 };
 
 
 
-int main()
+
+int main ()
 {
-	itemPricement* item = new itemPricement;
-	string value = "";
-	int value1=0;
-	float value2=0;
+  itemPricement *item = new itemPricement;
+  string value = "";
+  int value1 = 0;
+  float value2 = 0;
+  float finalprice=0.0;
+  
+  
+  cout << "Enter name of item\n";
+  cin >> value;
+  item->setName (value);
 
-	cout << "Enter name of item\n";
-	cin >> value;
-	item->setName(value);
+  cout << "Enter count of item\n";
+  cin >> value1;
+  item->setCount (value1);
 
-	cout << "Enter count of item\n";
-	cin >> value1;
-	item->setCount(value1);
-	
-	cout << "Enter price of item\n";
-	cin >> value2;
-	item->setPrice(value2 * item->getCount());
+  cout << "Enter price of item\n";
+  cin >> value2;
+  item->setPrice (value2);
 
-	cout << "Final price is: " << item->getPrice() << "$\n";
+    finalprice=item->getPrice()*item->getCount();
+  cout << "Final price is: " << finalprice << "$\n";
 
 
-	while (true)
+  while (true)
+    {
+      float temp = 0;
+      cout << "Input your money: \n";
+      cin >> temp;
+      if (temp >= finalprice)
 	{
-		float temp = 0;
-		cout << "Input your money: \n";
-		cin >> temp;
-		if (temp >= item->getPrice())
-		{
-			value2 = temp;
-			break;
-		}
-		else cout << "Not enough money\n\n";
+	  value2 = temp;
+	  break;
 	}
+      else
+	cout << "Not enough money\n\n";
+    }
 
-	if (value2 > item->getPrice())
-	{ 
-		cout << "There are too much money, take your change:\n";
-		cout << item->calcChange(value2)<<"$\n\n";
-	}
-	
-	fstream fin;
+  if (value2 > finalprice)
+    {
+      cout << "There are too much money, take your change:\n";
+      cout << item->calcChange (value2) << "$\n\n";
+    }
 
 
-	cout << "\t\t\tThanks for purchasing!";
-	
-	delete item;
+  ofstream fout;
+  if (!fout)
+    {
+      cout << "Open File error. Check File";
+    }
+
+
+  fout.open ("itemPricement.txt");
+  if (fout.is_open ())
+    {
+      fout << item->ToString ();
+    }
+
+
+  cout << "\t\t\tThanks for purchasing!";
+    
+  fout.close();
+  delete item;
 
 }
