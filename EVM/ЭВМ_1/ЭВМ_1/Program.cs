2545a6e4106cb7980bb61978dab9acc67e84425d
gingerswaltz <goshan712@mail.ru>
2023-02-22 21:56:06 +0900

    evm final

--- EVM/ЭВМ_1/ЭВМ_1/Program.cs
+++ EVM/ЭВМ_1/ЭВМ_1/Program.cs
@@ -2,15 +2,16 @@
 
 public static class  Fast
 {
-    public static decimal calcFast(int _a, int _b)
+    public static double calcFast(int _a, int _b)
     {
-        const int iter = 1000000;
+        const int iter = 10000000;
         int i = 0;
         int count = 0;
         Stopwatch stopWatch = new();
-        TimeSpan ts = new TimeSpan();
-        decimal sum = 0;
-        bool flag = false;
+        double time = 0;
+        var sum = 0.0;
+        
+        double timeDouble = 0;
        while (count <= 4)
         {
             count++;
@@ -22,17 +23,15 @@ public static class  Fast
             }
             
             stopWatch.Stop();
-            ts = stopWatch.Elapsed;
-            sum += stopWatch.ElapsedMilliseconds+ts.Minutes+ts.Seconds;
-            
+
+            time = stopWatch.ElapsedMilliseconds;
+      
+            time /= 1000; // получаем в секундах
+            time /= iter; // получаем сколько одна итерация выполняется
+            sum += time;
         }
-        
         sum /= 4;
-        return sum;
-
-
-
-
+        return 1/sum;
     }
 }
 
@@ -43,6 +42,6 @@ public static class MainFunc
 
         var result=Fast.calcFast(1, 1);
 
-        Console.WriteLine("Average time (ms) = "+result );
+        Console.WriteLine("Operations per sec = "+result );
     }
 }
