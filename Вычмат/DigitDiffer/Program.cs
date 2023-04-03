3b1a5e394c6347a38d7919c5377750125a3d3674
gingerswaltz <goshan712@mail.ru>
2023-04-03 19:13:38 +0900

    100% ready

--- Вычмат/DigitDiffer/Program.cs
+++ Вычмат/DigitDiffer/Program.cs
@@ -1,4 +1,6 @@
-﻿public static class Differentiation
+﻿using System.Globalization;
+
+public static class Differentiation
 {
     const double Pi = 3.141592654;
     public static double f(double x)
@@ -21,7 +23,43 @@
         return (diff1, diff2);
     }
 
+    public static void Solution2(double h)
+    {
+        const int n = 7;
+        double []x=new double[7];
+        double[,]y=new double[7,7];
 
+        for (int i = 0; i < 7; i++)
+        {
+            x[i] = 4+h*i;
+            y[i, 0] = Math.Pow((1 + x[i] * x[i]), 0.5);
+            
+        }
+
+        for (int j = 1; j < 7; j++)
+        {
+            for (int i = 0; i < n - j; i++)
+            {
+                y[i, j] = (y[i + 1, j - 1] - y[i, j - 1]);
+                string s1 = string.Format("{0:F6}", y[i, j]);
+                Console.WriteLine($"    y'x0={s1}"+'\t'+" ");
+            }
+            
+        }
+        Console.Write("\n");
+
+
+        double s = 0;
+        double d = -1;
+        double Delta = 0;
+        for (int j = 1; j < n - 1; j++)
+        {
+            d = (-1) * d;
+            s = s + d * y[0, j] / (j * h);
+            Delta = Math.Abs(y[0, j + 1]) / ((j + 1) * h);
+            Console.Write($"    Delta {j}:" + Delta+'\t'+" ");
+        }
+    }
 
     public static void Main(string[] args)
     {
@@ -31,9 +69,10 @@
         Console.WriteLine("Input h2: ");
         double.TryParse(Console.ReadLine(), out var h2);
         Console.WriteLine("Производная по 1 формуле: "+Solution1(h1, h2).Item1+"\n"+
-            "Производная по 2 формуле: " + Solution1(h1, h2).Item2);
+            "Производная по 2 формуле: " + Solution1(h1, h2).Item2+'\n');
 
         Console.WriteLine("Постановка 2");
+        Solution2(0.1);
 
     }
 }
