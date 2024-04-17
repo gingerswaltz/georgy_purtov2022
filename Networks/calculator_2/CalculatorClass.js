fc1408b68f8202fc30de4f18b7665c7ff066c0e9
gingerswaltz <goshan712@mail.ru>
2024-04-17 22:04:02 +0900

    aggregation
    ???

--- Networks/calculator_2/CalculatorClass.js
+++ Networks/calculator_2/CalculatorClass.js
@@ -143,4 +143,73 @@ class IpCalculator {
 
     return maxHost;
   }
+
+
+   // Метод для расчета подсетей
+   calculateSubnets(numSubnets) {
+    // Разделение маски подсети на отдельные части
+    const subnetParts = this.subnetMask.split(".").map((part) => parseInt(part));
+
+    // Вычисление числа единиц в обратной маске подсети
+    const numOnes = subnetParts.reduce((total, part) => {
+      const binaryPart = parseInt(part).toString(2);
+      return total + binaryPart.split("1").length - 1;
+    }, 0);
+
+    // Расчет числа доступных подсетей
+    const availableSubnets = Math.pow(2, numSubnets);
+
+    // Вычисление новой длины префикса подсети
+    const newPrefixLength = numOnes + numSubnets;
+
+    // Вычисление размера каждой подсети
+    const subnetSize = Math.pow(2, 32 - newPrefixLength);
+
+    // Вычисление сетевых адресов для каждой подсети
+    const subnets = [];
+    let networkAddress = this.calculateNetworkAddress();
+    for (let i = 0; i < availableSubnets; i++) {
+      subnets.push({
+        networkAddress: networkAddress,
+        broadcastAddress: this.calculatesubBroadcastAddress(networkAddress, newPrefixLength),
+        subnetMask: this.subnetMask,
+        prefixLength: newPrefixLength
+      });
+
+      // Переход к следующему сетевому адресу
+      networkAddress = this.incrementIpAddress(networkAddress, subnetSize);
+    }
+
+    return subnets;
+  }
+
+  // Метод для увеличения IP адреса на заданное количество хостов
+  incrementIpAddress(ipAddress, numHosts) {
+    const ipParts = ipAddress.split(".").map((part) => parseInt(part));
+    let increment = numHosts;
+    for (let i = ipParts.length - 1; i >= 0 && increment > 0; i--) {
+      ipParts[i] += increment;
+      increment = Math.floor(ipParts[i] / 256);
+      ipParts[i] %= 256;
+    }
+    return ipParts.join(".");
+  }
+
+  // Метод для вычисления широковещательного адреса заданной подсети
+  calculatesubBroadcastAddress(networkAddress, prefixLength) {
+    // Разделение сетевого адреса на отдельные части
+    const networkParts = networkAddress.split(".").map((part) => parseInt(part));
+
+    // Вычисление широковещательного адреса
+    const broadcastParts = [];
+    for (let i = 0; i < 4; i++) {
+      broadcastParts.push(networkParts[i] | (255 >> (32 - prefixLength)));
+    }
+
+    // Преобразование массива чисел в строку IP адреса
+    const broadcastAddress = broadcastParts.join(".");
+
+    return broadcastAddress;
+  }
 }
+
